97ce76086c501c601345d4b1027365b32e369b5d
Ian Doyle <iandoyle01@gmail.com>
2018-09-23 12:14:13 +0000

    refactored to create grid

--- boggle.py
+++ boggle.py
@@ -1,6 +1,8 @@
 def make_grid(width, height):
     """
-    Make an empty Boggle grid
+    Creates a grid that will hold all of the tiles
+    for a Boggle game
     """
-    return {}
+    return {(row, col): ' ' for row in range(height)
+        for col in range(width)}
     
