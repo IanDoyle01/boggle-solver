56edde3fb493da0b6055c1baea989fd4ef2f69d3
Ian Doyle <iandoyle01@gmail.com>
2018-09-23 12:26:39 +0000

    added letters to grid coordinates

--- boggle.py
+++ boggle.py
@@ -1,8 +1,12 @@
+from string import ascii_uppercase #alphabet
+from random import choice #random choice
+
 def make_grid(width, height):
     """
     Creates a grid that will hold all of the tiles
     for a Boggle game
     """
-    return {(row, col): ' ' for row in range(height)
+    return {(row, col): choice(ascii_uppercase) 
+        for row in range(height)
         for col in range(width)}
     
